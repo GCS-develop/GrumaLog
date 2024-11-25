e02238b4fefe447f9d7392f373936dd3d8f02ddc
Victor <victorburbanoherpo@gmail.com>
2024-11-25 08:39:42 -0500

    cambio nombre impresora

--- frontend/modules/productostiquetesprecio/views/Impresoraspaxarbodega/update.php
+++ frontend/modules/productostiquetesprecio/views/Impresoraspaxarbodega/update.php
@@ -5,14 +5,12 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var frontend\models\Impresoraspaxarbodega $model */
 
-$this->title = 'Update Impresoraspaxarbodega: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Impresoraspaxarbodegas', 'url' => ['index']];
+$this->title = 'Actualizar Impresora paxar: ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Impresoras paxar', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="impresoraspaxarbodega-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
