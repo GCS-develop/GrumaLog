28767bc685a60403a0f3d36578839994f6601964
Victor <victorburbanoherpo@gmail.com>
2025-04-30 10:35:57 -0500

    Modificacion del query de analisis de venta

--- frontend/modules/ventas/views/analisisventa/index.php
+++ frontend/modules/ventas/views/analisisventa/index.php
@@ -1,5 +1,5 @@
 <?php
-$reportId = "eyJrIjoiYWM4N2M2MjktZTEyMC00OTU5LWIyMjgtNzgwY2M1MTczZjFkIiwidCI6ImM1ZWYwODEzLWUxOGQtNGY1ZS1iZTA0LTg4OTFkYWE3ODBlMCJ9";
+$reportId = "eyJrIjoiYmZiYTVlOTUtZTlmMC00Y2ZjLWEzMDAtOTExMDM0ZjJkY2M1IiwidCI6ImM1ZWYwODEzLWUxOGQtNGY1ZS1iZTA0LTg4OTFkYWE3ODBlMCJ9";
 $filter = urlencode("GnralData/TIENDA eq 'JAMUNDI'"); // Asegúrate de usar el nombre real de la tabla y campo en Power BI
 $url = "https://app.powerbi.com/view?r=$reportId&filter=$filter";
 ?>
@@ -7,6 +7,3 @@ $url = "https://app.powerbi.com/view?r=$reportId&filter=$filter";
 <iframe title="Informe de Ventas Grupo Mayorista" width="1140" height="541.25" src="<?= $url ?>" frameborder="0"
     allowFullScreen="true">
 </iframe>
-
-
-<!-- src="https://app.powerbi.com/view?r=eyJrIjoiYWM4N2M2MjktZTEyMC00OTU5LWIyMjgtNzgwY2M1MTczZjFkIiwidCI6ImM1ZWYwODEzLWUxOGQtNGY1ZS1iZTA0LTg4OTFkYWE3ODBlMCJ9&pageName=ReportSection2"  -->
